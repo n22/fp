6974973f956c88819808365c7517b4ebb858e81c
n22 <hanifnarendra@gmail.com>
2016-01-03 15:28:58 +0700

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.1.1'
+gem 'eventmachine'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -11,7 +12,8 @@ gem 'devise'
 gem 'private_pub'
 gem 'thin'
 gem 'sdoc', '~> 0.4.0',          group: :doc
-
+gem 'attr_encrypted'
+gem 'carrierwave'
 group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730185251) do
+ActiveRecord::Schema.define(version: 20160102084836) do
 
   create_table "conversations", force: true do |t|
     t.integer  "sender_id"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140730185251) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gambar"
+    t.string   "body_encrypted"
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
